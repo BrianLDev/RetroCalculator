9cbd5dddfcd140ec0692adc129b729e77e54df67
Brian Leip <BrianLeip@gmail.com>
2016-10-08 10:24:25 -0700

    Retro Calc done, also added Clear and decimal point buttons
    Fully functioning retro calculator app.
    Extended it to add Clear button and also a Decimal Point button.
    Ideas for future improvements:
    Rearrange buttons to make them more intuitive.
    Vary the layout for different size classes

--- 48-RetroCalculator/ViewController.swift
+++ 48-RetroCalculator/ViewController.swift
@@ -7,17 +7,134 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    var hasDecimalPoint = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        outputLbl.text = "0"
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed(sender: UIButton)  // this is linked to all number buttons on the calculator app
+    {
+        playSound()
+        runningNumber += "\(sender.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
+        hasDecimalPoint = false
+    }
+    
+    @IBAction func onDecimalPointPressed(sender: AnyObject) {
+        playSound()
+        if (!hasDecimalPoint) {
+            runningNumber += "."
+            hasDecimalPoint = true
+        }
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        playSound()
+        runningNumber = ""
+        leftValStr = ""
+        rightValStr = ""
+        result = "0"
+        outputLbl.text = result
+        currentOperation = Operation.Empty
+        hasDecimalPoint = false
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            
+            // A user selected an operator but then selected another operator without first entering a number
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLbl.text = result
+            }
+            
+            currentOperation = operation
+            
+        } else {
+            // This is the first time an operator has been pressed
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+        
+        hasDecimalPoint = false
     }
 
 
